276beef28052fc94275c76453651557c37133fb9
joseph517 <alvarovergara1993@hotmail.com>
2023-04-20 16:57:07 -0500

    avance en el modelo types

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin, register
-from .models import Product
+from .models import Product, Type
 
 # Register your models here.
 
@@ -11,6 +11,13 @@ class ProductAdmin(ModelAdmin):
         'id_producto',
         'nombre_producto',
         'descripcion_producto',
-        'precio_producto'
+        'precio_producto',
     )
     search_fields = ('nombre_producto',)
+
+@register(Type)
+class TypeAdmin(ModelAdmin):
+    list_display = (
+        'nombre',
+    )
+    search_fields = ('nombre',)
--- apps/product/models.py
+++ apps/product/models.py
@@ -1,13 +1,54 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 
+class Type(models.Model):
+    nombre = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        default=None,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = "tipo"
+        verbose_name_plural = "tipos"
+
 
 class Product(models.Model):
     id_producto = models.AutoField(primary_key=True)
-    nombre_producto = models.CharField(max_length=200)
-    descripcion_producto = models.TextField()
-    precio_producto = models.DecimalField(max_digits=10, decimal_places=2)
+    nombre_producto = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        default=None,
+        unique=True
+    )
+
+    descripcion_producto = models.TextField(
+        null=False,
+        blank=False,
+        default=None
+    )
+
+    precio_producto = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        default=None
+    )
+
+    tipo_producto = models.ForeignKey(Type, on_delete=models.CASCADE, null=False, blank=True)
+
+    created_at = models.DateTimeField(
+        verbose_name="Fecha de Creación",
+        default=timezone.now,
+    )
+    updated_at = models.DateTimeField(default=timezone.now)
+
 
     class Meta:
         verbose_name = "Producto"
